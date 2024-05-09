58d6478ea893a8f5eb1fee2a0d5f31d29ee9ecbc
Mech <stefrensma@gmail.com>
2024-05-09 17:14:46 +0200

    use xampp database

---
+++ src/dao/mysql-db.js
@@ -0,0 +1,48 @@
+const mysql = require('mysql2')
+const logger = require('../../src/util/logger')
+require('dotenv').config()
+
+// Set the log level to the value of the LOG_LEVEL environment variable
+// Only here to show how to set the log level
+const tracer = require('tracer')
+tracer.setLevel(process.env.LOG_LEVEL)
+
+// Hier worden de db connection settings opgehaald uit de .env file
+const dbConfig = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+
+    connectionLimit: 10,
+    waitForConnections: true,
+    queueLimit: 0,
+    multipleStatements: true
+}
+
+logger.trace(dbConfig)
+
+// Hier wordt de pool aangemaakt
+const pool = mysql.createPool(dbConfig)
+
+//
+// Hier worden de events van de pool gelogd, zodat je kunt zien wat er gebeurt
+//
+pool.on('connection', function (connection) {
+    logger.trace(
+        `Connected to database '${connection.config.database}' on '${connection.config.host}:${connection.config.port}'`
+    )
+})
+
+pool.on('acquire', function (connection) {
+    logger.trace('Connection %d acquired', connection.threadId)
+})
+
+pool.on('release', function (connection) {
+    logger.trace('Connection %d released', connection.threadId)
+})
+
+
+// Export the pool, so that testcases can use it
+module.exports = pool
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,89 +1,180 @@
 const database = require('../dao/inmem-db')
 const logger = require('../util/logger')
-
+const db = require('../dao/mysql-db.js')
 const userService = {
     create: (user, callback) => {
-        logger.info('create user', user)
-        database.add(user, (err, data) => {
+        logger.info('create user', user);
+
+        db.getConnection(function (err, connection) {
             if (err) {
-                logger.info(
-                    'error creating user: ',
-                    err.message || 'unknown error'
-                )
-                callback(err, null)
-            } else {
-                logger.trace(`User created with id ${data.id}.`)
-                callback(null, {
-                    message: `User created with id ${data.id}.`,
-                    data: data
-                })
+                logger.error(err);
+                callback(err, null);
+                return;
             }
-        })
+
+            const { firstName, lastName, isActive, emailAddress, password, phoneNumber, roles, street, city } = user;
+
+            connection.query(
+                'INSERT INTO user (firstName, lastName, isActive, emailAddress, password, phoneNumber, roles, street, city) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                [firstName, lastName, isActive, emailAddress, password, phoneNumber, roles, street, city],
+                function (error, results, fields) {
+                    connection.release();
+
+                    if (error) {
+                        logger.error('error creating user:', error.message || 'unknown error');
+                        callback(error, null);
+                    } else {
+                        logger.trace('User created.');
+                        callback(null, {
+                            message: 'User created.',
+                            data: results
+                        });
+                    }
+                }
+            );
+        });
     },
+
 
     getAll: (callback) => {
         logger.info('getAll')
-        database.getAll((err, data) => {
+        db.getConnection(function (err, connection) {
             if (err) {
+                logger.error(err)
                 callback(err, null)
-            } else {
-                callback(null, {
-                    message: `Found ${data.length} users.`,
-                    data: data
-                })
+                return
             }
+
+            connection.query(
+                'SELECT * FROM `user`',
+                function (error, results, fields) {
+                    connection.release()
+
+                    if (error) {
+                        logger.error(error)
+                        callback(error, null)
+                    } else {
+                        logger.debug(results)
+                        callback(null, {
+                            message: `Found ${results.length} users.`,
+                            data: results
+                        })
+                    }
+                }
+            )
         })
     },
     getById: (userId, callback) => {
         logger.info('getById');
-        database.getById(userId, (err, data) => {
+        db.getConnection(function (err, connection) {
             if (err) {
-                callback(err, null);
-            } else {
-                callback(null, {
-                    message: `Found user with id ${data.id}.`,
-                    data: data
-                });
+                logger.error(err)
+                callback(err, null)
+                return
             }
-        });
-    }
-    ,
+
+            connection.query(
+                'SELECT * FROM `user` WHERE id = ?',
+                id = userId,
+                function (error, results, fields) {
+                    connection.release()
+
+                    if (error) {
+                        logger.error(error)
+                        callback(error, null)
+                    } else {
+                        logger.debug(results)
+                        callback(null, {
+                            message: `Found user.`,
+                            data: results
+                        })
+                    }
+                }
+            )
+        })
+    },
     delete: (userId, callback) => {
         logger.info('delete user', userId)
-        database.delete(userId, (err, data) => {
+        db.getConnection(function (err, connection) {
             if (err) {
-                logger.info(
-                    'error deleting user: ',
-                    err.message || 'unknown error'
-                )
+                logger.error(err)
                 callback(err, null)
-            } else {
-                logger.trace(`User deleted with id ${data.id}.`)
-                callback(null, {
-                    message: `User deleted with id ${data.id}.`,
-                    data: data
-                })
+                return
             }
+
+            connection.query(
+                'DELETE FROM `user` WHERE id = ?',
+                id = userId,
+                function (error, results, fields) {
+                    connection.release()
+
+                    if (err) {
+                        logger.info(
+                            'error deleting user: ',
+                            err.message || 'unknown error'
+                        )
+                        callback(err, null)
+                    } else {
+                        logger.trace(`User deleted with id ${userId}.`)
+                        callback(null, {
+                            message: `User deleted with id ${userId}.`,
+                            data: results
+                        })
+                    }
+                }
+            )
         })
     },
     update: (userId, user, callback) => {
-        logger.info('update user', userId)
-        database.update(userId, user, (err, data) => {
+        logger.info('update user', userId);
+
+        const valuesToUpdate = [];
+        const columnsToUpdate = Object.keys(user)
+            .filter(key => user[key] !== undefined && user[key] !== null) // Filter out undefined or null values
+            .map(key => {
+                valuesToUpdate.push(user[key]);
+                return `${key}=?`;
+            });
+
+        if (columnsToUpdate.length === 0) {
+            // No fields to update
+            callback(new Error('No fields to update'), null);
+            return;
+        }
+
+        const setClause = columnsToUpdate.join(', ');
+        const sql = `UPDATE user SET ${setClause} WHERE id = ?`;
+
+        db.getConnection(function (err, connection) {
             if (err) {
-                logger.info(
-                    'error updating user: ',
-                    err.message || 'unknown error'
-                )
-                callback(err, null)
-            } else {
-                logger.trace(`User updated with id ${data.id}.`)
-                callback(null, {
-                    message: `User updated with id ${data.id}.`,
-                    data: data
-                })
+                logger.error(err);
+                callback(err, null);
+                return;
             }
-        })
+
+            const values = [...valuesToUpdate, userId];
+
+            connection.query(
+                sql,
+                values,
+                function (error, results, fields) {
+                    connection.release();
+
+                    if (error) {
+                        logger.error('Error updating user:', error.message || 'unknown error');
+                        callback(error, null);
+                    } else {
+                        logger.trace(`User updated with id ${userId}.`);
+                        callback(null, {
+                            message: `User updated with id ${userId}.`,
+                            data: results
+                        });
+                    }
+                }
+            );
+        });
     }
+
 }
 
 module.exports = userService
